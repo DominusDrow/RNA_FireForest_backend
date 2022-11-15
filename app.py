cb24d5ef70e8607487e6cd05f5e9c7297b1efa0d
DominusDrow <galacticovasquez@gmail.com>
2022-11-14 19:17:46 -0600

    backend rna

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import sklearn
 
 #curl -d "{\"Medidas\":[[1,2,3,4]]}" -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/predecir
 
-
 app= Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -20,7 +18,6 @@ def predecir():
     clf=joblib.load('fireforestDetect.pkl')
     prediccion=clf.predict(medidas)
     return 'Las medidas que diste corresponden a la clase {0}\n\n'.format(prediccion)
-
 
 
 if __name__ == '__main__':
