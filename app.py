780a7ca8b6b884e4d0c5f99ad9f4f4ce87bdd8e7
DominusDrow <galacticovasquez@gmail.com>
2022-11-16 05:21:08 -0600

    cors security

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from joblib import load
 import sklearn
+
 
 #curl -d "{\"Medidas\":[[1,2,3,4]]}" -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/predecir
 
 app= Flask(__name__)
 
+CORS(app)
+
 @app.route("/")
 def home():
     return 'La pagina esta funcionando bien'
 
-
+@cross_origin
 @app.route("/predecir", methods=["POST"])
 def predecir():
 
